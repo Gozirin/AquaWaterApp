3d94f0095b065ffd4c6e17d71ca701b3e322517b
Isaac Uduehi <osayi4jesus@gmail.com>
2022-05-06 23:56:38 +0100

    SplashScreen and Nav component

--- app/src/main/java/com/decagon/aqua/MainActivity.kt
+++ app/src/main/java/com/decagon/aqua/MainActivity.kt
@@ -2,10 +2,13 @@ package com.decagon.aqua
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.decagon.aqua.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
