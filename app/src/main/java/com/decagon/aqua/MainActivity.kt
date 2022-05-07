d93c841ff80643762e7dc9f93c93794bd8082d92
osarinmwian <noel.osarinmwian@gmail.com>
2022-05-07 19:43:50 +0100

    sign up fragment added

--- app/src/main/java/com/decagon/aqua/MainActivity.kt
+++ app/src/main/java/com/decagon/aqua/MainActivity.kt
@@ -2,10 +2,17 @@ package com.decagon.aqua
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.decagon.aqua.databinding.ActivityMainBinding
+import com.decagon.aqua.features.consumer.authentication.CustomerLoginFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // initializing support fragment manager
+        supportFragmentManager.beginTransaction().replace(R.id.nav_Container, CustomerLoginFragment())
+            .commit()
     }
 }
---
+++ app/src/main/java/com/decagon/aqua/features/consumer/authentication/CustomerLoginFragment.kt
@@ -0,0 +1,35 @@
+package com.decagon.aqua.features.consumer.authentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.decagon.aqua.R
+import com.decagon.aqua.databinding.FragmentLoginConsumerBinding
+class CustomerLoginFragment : Fragment() {
+    private lateinit var binding:FragmentLoginConsumerBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = inflater.inflate(R.layout.fragment_login_consumer, container, false)
+        return binding.rootView
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //initializing view binding
+        binding = FragmentLoginConsumerBinding.bind(view)
+        //Sign up if consumer don't have an account
+        val nextbtn = binding.textLoginToSignUp
+        nextbtn.setOnClickListener {
+            val fragment = ConsumerSignUpFragment() // navigate to login fragment
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.nav_Container, fragment)?.commit()
+
+        }
+
+    }
+}
