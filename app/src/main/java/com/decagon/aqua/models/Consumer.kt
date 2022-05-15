5beb53a1b55b7f137b04c8ce93a52eafa2771748
Stryker101 <onuoha.henrychika@gmail.com>
2022-05-15 16:54:35 +0100

    roomDB setup

--- app/src/main/java/com/decagon/aqua/models/Consumer.kt
+++ app/src/main/java/com/decagon/aqua/models/Consumer.kt
@@ -1,15 +1,15 @@
 package com.decagon.aqua.models
 
-// import androidx.room.Entity
-// import androidx.room.PrimaryKey
-//
-// @Entity(
-//    tableName = "consumers"
-// )
-// data class Consumer(
-//    @PrimaryKey(autoGenerate = true)
-//    var consumerID: Int,
-//    val consumptionLevel: Int,
-//    val earnedCash: Int,
-//    val user: User
-// )
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "consumers"
+)
+data class Consumer(
+    @PrimaryKey(autoGenerate = true)
+    var consumerID: Int,
+    val consumptionLevel: Int,
+    val earnedCash: Int,
+    val user: User
+)
