20711bb80ce31b97d3ef9dc66ce3b5cf62be4523
Stryker101 <onuoha.henrychika@gmail.com>
2022-05-16 03:22:27 +0100

    supplier profile screen added

--- app/src/main/java/com/decagon/aqua/feature/supplier/ui/SupplierAccountPage.kt
+++ app/src/main/java/com/decagon/aqua/feature/supplier/ui/SupplierAccountPage.kt
@@ -5,18 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.decagon.aqua.R
+import com.decagon.aqua.databinding.FragmentSupplierAccountLayoutBinding
 
 class SupplierAccountPage : Fragment() {
+
+    private lateinit var binding: FragmentSupplierAccountLayoutBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_supplier_account_layout, container, false)
+    ): View {
+        binding = FragmentSupplierAccountLayoutBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.supplierAccountFragmentEditProfileTv.setOnClickListener {
+            findNavController().navigate(R.id.action_supplierAccountPage_to_supplierProfileDetailsFragment)
+        }
     }
 }
