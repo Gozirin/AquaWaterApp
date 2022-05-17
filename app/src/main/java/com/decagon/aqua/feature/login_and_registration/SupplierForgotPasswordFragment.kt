a6f673ae8aa4debe4d033214e205ce745e1eebf0
Gozirin <chigozp2@gmail.com>
2022-05-17 09:58:13 +0100

    forgot password screen added

--- app/src/main/java/com/decagon/aqua/feature/login_and_registration/SupplierForgotPasswordFragment.kt
+++ app/src/main/java/com/decagon/aqua/feature/login_and_registration/SupplierForgotPasswordFragment.kt
@@ -5,15 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.decagon.aqua.R
+import com.decagon.aqua.databinding.FragmentSupplierForgotPasswordBinding
+
 class SupplierForgotPasswordFragment : Fragment() {
 
+    private lateinit var binding: FragmentSupplierForgotPasswordBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_supplier_forgot_password, container, false)
+        val binding = inflater.inflate(R.layout.fragment_supplier_forgot_password, container, false)
+        return binding.rootView
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // initializing view binding
+        binding = FragmentSupplierForgotPasswordBinding.bind(view)
+
+        // navigate to consumer forgot password  page
+        binding.textView3.setOnClickListener {
+            findNavController().navigate(R.id.action_supplierForgotPasswordFragment_to_consumerForgotPasswordFragment)
+        }
     }
 }
