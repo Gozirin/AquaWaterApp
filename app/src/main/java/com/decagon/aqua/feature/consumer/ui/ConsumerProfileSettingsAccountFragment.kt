45330d04315affb2479beef672e47ae01521dcd0
TonyoGodson <59567558+TonyoGodson@users.noreply.github.com>
2022-05-20 14:18:38 +0100

    consumer profile account screen added

---
+++ app/src/main/java/com/decagon/aqua/feature/consumer/ui/ConsumerProfileSettingsAccountFragment.kt
@@ -0,0 +1,29 @@
+package com.decagon.aqua.feature.consumer.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.decagon.aqua.R
+import com.decagon.aqua.databinding.FragmentConsumerProfileSettingsAccountBinding
+
+class ConsumerProfileSettingsAccountFragment : Fragment() {
+    private lateinit var binding: FragmentConsumerProfileSettingsAccountBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.fragment_consumer_profile_settings_account,
+            container,
+            false
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentConsumerProfileSettingsAccountBinding.bind(view)
+    }
+}
