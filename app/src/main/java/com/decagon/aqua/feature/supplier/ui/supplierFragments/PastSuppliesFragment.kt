966cbf89bb190c783b696d10b917d43c4760d904
Isaac Uduehi <osayi4jesus@gmail.com>
2022-05-24 22:23:17 +0100

    Validate consumer sign up

--- app/src/main/java/com/decagon/aqua/feature/supplier/ui/supplierFragments/PastSuppliesFragment.kt
+++ app/src/main/java/com/decagon/aqua/feature/supplier/ui/supplierFragments/PastSuppliesFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
