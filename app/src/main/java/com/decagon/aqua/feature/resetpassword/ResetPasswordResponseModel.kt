d26e5830c36c1bee29267fcd55a78015c6323ae2
osarinmwian <noel.osarinmwian@gmail.com>
2022-06-02 03:03:48 +0100

    resolve conflict

--- app/src/main/java/com/decagon/aqua/feature/resetpassword/ResetPasswordResponseModel.kt
+++
@@ -1,8 +0,0 @@
-package com.decagon.hbapplicationgroupa.model.authmodule.resetpassword
-
-data class ResetPasswordResponseModel(
-    val statusCode: String,
-    val success: Boolean,
-    val Message: String,
-    val errors: String?
-)
