63d2be296d22e4955d6711f2c8c145710b1cb5e2
Isaac Uduehi <osayi4jesus@gmail.com>
2022-06-09 09:49:05 +0100

    Validation and Implementation of consumer sign up Updated

--- app/src/main/java/com/decagon/aqua/feature/consumer/authentication/AquaRepositoryInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.decagon.aqua.feature.consumer.authentication
-
-import com.decagon.aqua.core.data.UserSignUpResponse
-import com.decagon.aqua.models.Consumer
-import com.decagon.aqua.utils.Resource
-
-interface AquaRepositoryInterface {
-    suspend fun signUp(signUpRequest: Consumer): Resource<UserSignUpResponse>
-}
