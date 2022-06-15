b9a8bbd6ae3d943bd96e6766de1bbc0d8d6d8cfb
Isaac Uduehi <osayi4jesus@gmail.com>
2022-06-15 09:51:44 +0100

    revert "Get orders"
    This reverts commit 23d6b49bfa6254b51c03fd69e4f19b49b20a842d.
    
    get orders

--- app/src/main/java/com/decagon/aqua/models/consumerAuthModule/getcompanieswithfeaturedproduct/PageItem.kt
+++
@@ -1,7 +0,0 @@
-package com.decagon.aqua.models.consumerAuthModule.getcompanieswithfeaturedproduct
-
-data class PageItem(
-    val companyName: String,
-    val location: Location,
-    val product: Product? = null
-)
