f8195aef0dfa9cfb02be6d962fa445022a174e4a
Osehi <osehiaseehilen@gmail.com>
2022-06-16 03:37:41 +0100

    implement-preference

--- app/src/main/java/com/decagon/aqua/core/data/sharedpreference/AquaPreferences.kt
+++ app/src/main/java/com/decagon/aqua/core/data/sharedpreference/AquaPreferences.kt
@@ -1,4 +1,22 @@
 package com.decagon.aqua.core.data.sharedpreference
 
-class AquaPreferences {
+import android.content.Context
+import javax.inject.Inject
+
+class AquaPreferences @Inject constructor(context: Context) : Preference {
+
+    companion object {
+        const val PREFERENCES_NAME = "AQUA_PREFERENCES"
+    }
+
+    private val preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+
+    override fun putToken(token: String) {
+        preferences.edit().putString(PreferencesConstants.KEY_TOKEN, token).apply()
+    }
+
+    override fun getToken(): String {
+        return preferences.getString(PreferencesConstants.KEY_TOKEN, "").orEmpty()
+    }
+
 }
