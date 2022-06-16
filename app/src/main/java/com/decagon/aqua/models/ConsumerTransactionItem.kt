d82e45b0e5a1e206266f4d95dcfeb1dff0e0a632
osarinmwian <noel.osarinmwian@gmail.com>
2022-06-16 16:24:05 +0100

    transaction screen

---
+++ app/src/main/java/com/decagon/aqua/models/ConsumerTransactionItem.kt
@@ -0,0 +1,13 @@
+package com.decagon.aqua.models
+
+import com.decagon.aqua.R
+
+class ConsumerTransactionItem {
+    var consumerTransactionItem = mutableListOf(
+        TransactionData("noel", "#20,000", "200", "22/12/2022", R.drawable.water_circle),
+        TransactionData("noel", "#20,000", "200", "22/12/2022", R.drawable.despensablewater),
+        TransactionData("noel", "#20,000", "200", "22/12/2022", R.drawable.aquawaterbottle),
+        TransactionData("noel", "#20,000", "200", "22/12/2022", R.drawable.aquawaterbrand),
+
+    )
+}
---
+++ app/src/main/java/com/decagon/aqua/models/TransactionData.kt
@@ -0,0 +1,10 @@
+package com.decagon.aqua.models
+
+data class TransactionData(
+    var name: String,
+    var amount: String,
+    var quantity: String,
+    var date: String,
+    val imgResource: Int,
+
+)
