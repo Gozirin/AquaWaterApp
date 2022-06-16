c6e2b657387d8a79e365061fb0e114b629a11035
Isaac Uduehi <osayi4jesus@gmail.com>
2022-06-16 10:46:06 +0100

    resolved conflicts

--- app/src/main/java/com/decagon/aqua/feature/supplier/adapter/Adapter.kt
+++ app/src/main/java/com/decagon/aqua/feature/supplier/adapter/Adapter.kt
@@ -4,8 +4,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.decagon.aqua.feature.supplier.OrderFragment
 import com.example.tablayout.fragment.IncomingFragment
-import com.example.tablayout.fragment.OrderFragment
+// import com.example.tablayout.fragment.OrderFragment
 
 class Adapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle) {
     override fun getItemCount(): Int {
